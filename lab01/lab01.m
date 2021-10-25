aa37059819038fe4edf340c8ca4af6db4d4c398a
Kerimov Ahmed <kerimov.dev@yandex.ru>
2021-10-25 23:30:32 +0300

    Some fixes

--- lab01/lab01.m
+++ lab01/lab01.m
@@ -20,19 +20,14 @@ function lab01
 	disp('Матрица стоимостей:');
 	disp(C);
 
-	% Проверка квадратности матрицы стоимостей
+	% Проверка матрицы
 	[height, width] = size(C);
-	if height ~= width
-		disp('Матрица не квадратная!');
+	if height ~= width || height == 0
+		disp('Неправильный размер матрицы!');
 		return;
 	end
 
-	% Проверка размерности матрицы стоимостей
 	n = height;
-	if n == 0
-		disp('Матрица нулевой размерности!');
-		return;
-	end
 
 	Ct = C;
 
@@ -42,10 +37,9 @@ function lab01
 
 		Ct = -Ct + max(Ct, [], 'all');
 
-		debug_disp('Č =');
+		debug_disp('C̃ =');
 		debug_disp(Ct);
 	end
-
 
 	debug_disp('[I] Подготовительный этап');
 	debug_disp('1. Из каждого столбца матрицы вычтем его наименьший элемент');
@@ -55,9 +49,8 @@ function lab01
 
 	debug_disp('Наименьшие элементы в столбцах матрицы стоимостей:');
 	debug_disp(minInColumns);
-	debug_disp('Č =');
+	debug_disp('C̃ =');
 	debug_disp(Ct);
-
 
 	debug_disp('2. Из каждой строки матрицы вычтем её наименьший элемент');
 
@@ -69,45 +62,34 @@ function lab01
 	debug_disp('Č =');
 	debug_disp(Ct);
 
-
 	debug_disp('3. Строим начальную СНН:');
 	debug_disp('Просмотрим столбцы текущей матрицы стоимостей (в порядке возрастания номера столбца) сверху вниз.');
-	debug_disp('Первый в строке нуль, в одной строке с которым нет 0*, отмечаем 0*.');
+	debug_disp('Первый в столбце нуль, в одной строке с которым нет 0*, отмечаем 0*.');
 
 	stars = initStars(Ct, n);
 	strokes = false(n);
 	colsBusy = false([1 n]);
 	rowsBusy = false([n 1]);
 
-	debug_disp('Č =');
+	debug_disp('C̃ =');
 	debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
-
 
 	debug_disp('4. k := |СНН|');
 
 	k = sum(stars, 'all');
-
 	debug_fprintf('k = %d\n\n', k);
-
 
 	debug_disp('[II] Основной этап');
 
-	found_h = false;
 	iteration = 1;
 	while k ~= n
-		if ~found_h
-			debug_fprintf('-- Итерация %d\n', iteration);
-		end
-		found_h = false;
-
+		debug_fprintf('-- Итерация %d\n', iteration);
 		debug_disp('5. Столбцы с 0* отмечаем "+"');
 
 		colsBusy = fillColsBusy(colsBusy, stars, n);
-		rowsBusy(:) = false;
 
-		debug_disp('Č =');
+		debug_disp('C̃ =');
 		debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
-
 
 		runInnerWhile = true;
 		while runInnerWhile
@@ -121,9 +103,8 @@ function lab01
 
 						strokes(row, col) = true;
 
-						debug_disp('Č =');
+						debug_disp('C̃ =');
 						debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
-
 
 						idx = find(stars(row, :), 1);
 						if ~isempty(idx)
@@ -133,13 +114,12 @@ function lab01
 							colsBusy(idx) = false;
 							rowsBusy(row) = true;
 
-							debug_disp('Č =');
+							debug_disp('C̃ =');
 							debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
 
 							runInnerWhile = true;
 							break;
 						end
-
 
 						debug_disp("8. В одной строке с текущим 0' нет 0*, поэтому");
 						debug_disp("строим непродолжаемую L-цепочку: от текущего 0' по столбцу в 0* по строке ... по строке в 0'");
@@ -149,21 +129,20 @@ function lab01
 						debug_disp('L-цепочка [row col]:');
 						debug_disp(Lchain);
 
-
 						debug_disp("9. В пределах L-цепочки меняем 0* на 0, а 0' на 0*");
 
 						[stars, strokes] = processLchain(stars, strokes, Lchain);
 
-						debug_disp('Č =');
+						debug_disp('C̃ =');
 						debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
-
 
 						debug_disp("10. Снимаем все выделения, k := |СНН|");
 
 						colsBusy(:) = false;
 						rowsBusy(:) = false;
+						strokes(:) = false;
 
-						debug_disp('Č =');
+						debug_disp('C̃ =');
 						debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
 
 						k = sum(stars, 'all');
@@ -188,30 +167,27 @@ function lab01
 
 				debug_disp('Вычтем h из невыделенных столбцов.');
 				Ct(:, ~colsBusy) = Ct(:, ~colsBusy) - h;
-				debug_disp('Č =');
-				debug_disp(Ct);
+				debug_disp('C̃ =');
+				debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
 
 				debug_disp('Добавим h к выделенным строкам.');
 				Ct(rowsBusy, :) = Ct(rowsBusy, :) + h;
-				debug_disp('Č =');
-				debug_disp(Ct);
+				debug_disp('C̃ =');
+				debug_disp_matrix(Ct, stars, strokes, colsBusy, rowsBusy);
 
-				found_h = true;
-				iteration = iteration - 1;
+				runInnerWhile = true;
 			end
 		end
 
 		iteration = iteration + 1;
 	end
 
-
 	debug_disp('12. k = n, запишем оптимальное решение');
-
 	disp('Оптимальное решение: X* =');
 	disp(stars);
 
 	f = sum(C .* stars, 'all');
-	fprintf('f* = %d\n', f);
+	fprintf('Стоимость: f* = %d\n', f);
 end
 
 function stars = initStars(Ct, n)
