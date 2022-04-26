4740f1c8123b011a45c2fea157b1be2b49860249
Kerimov Ahmed <kerimov.dev@yandex.ru>
2022-04-26 21:08:27 +0300

    Update lab02.m

--- sem10/lab02/lab02.m
+++ sem10/lab02/lab02.m
@@ -1,9 +1,10 @@
 function lab02
+    clc;
     debug = true;
 
     a = 0;
     b = 1;
-    eps = 1e-2;
+    eps = 1e-6;
 
     fplot(@(x) func(x), [a, b], 'b');
     hold on;
@@ -12,8 +13,9 @@ function lab02
     N = 0;
 
     [x, f] = goldenSectionSearch(a, b, eps, debug);
-    fprintf('Минимум функции: (x=%10.8f, f=%10.8f)\n', x, f);
+    fprintf('Минимум функции: (x=%10.10f, f=%10.10f)\n', x, f);
     fprintf('N = %d\n', N);
+    x - 0.777
 
     p = plot(x, f, 'rx', 'MarkerSize', 15);
     legend((p), 'Метод золотого сечения', 'Location', 'northwest');
@@ -25,14 +27,16 @@ function y = func(x)
     global N;
     N = N + 1;
 
-    x3 = power(x, 3);
-    x2 = power(x, 2);
-    sqrt2 = sqrt(2);
+    y = (x - 0.777) .^ 14;
 
-    ch = cosh((3 * x3 + 2 * x2 - 4 * x + 5) / 3);
-    th = tanh((x3 - 3 * sqrt2 * x - 2) / (2 * x + sqrt2));
-
-    y = ch + th - 2.5;
+%     x3 = power(x, 3);
+%     x2 = power(x, 2);
+%     sqrt2 = sqrt(2);
+%
+%     ch = cosh((3 * x3 + 2 * x2 - 4 * x + 5) / 3);
+%     th = tanh((x3 - 3 * sqrt2 * x - 2) / (2 * x + sqrt2));
+%
+%     y = ch + th - 2.5;
 end
 
 function [x, f] = goldenSectionSearch(a, b, eps, debug)
@@ -45,13 +49,18 @@ function [x, f] = goldenSectionSearch(a, b, eps, debug)
     fr = func(xr);
 
     iteration = 1;
-    while (delta > 2 * eps)
-        if (debug)
-            fprintf('Итерация %d: [a=%10.8f, b=%10.8f], (xl=%10.8f, xr=%10.8f)\n', iteration, a, b, xl, xr);
-            iteration = iteration + 1;
-        end
+    colors = ['k', 'r', 'b'];
 
-        if (fl > fr)
+    if debug
+        fprintf('Итерация %d: [a=%10.10f, b=%10.10f], (xl=%10.10f, xr=%10.10f)\n', iteration, a, b, xl, xr);
+        plot([xl xr], [fl fr], colors(1 + mod(iteration - 1, 3)));
+        plot(xl, fl, 'k.', 'MarkerSize', 10);
+        plot(xr, fr, 'k.', 'MarkerSize', 10);
+        iteration = iteration + 1;
+    end
+
+    while delta > 2 * eps
+        if fl > fr
             a = xl;
             delta = b - a;
             xl = xr;
@@ -65,6 +74,14 @@ function [x, f] = goldenSectionSearch(a, b, eps, debug)
             fr = fl;
             xl = b - tau * delta;
             fl = func(xl);
+        end
+
+        if debug
+            fprintf('Итерация %d: [a=%10.10f, b=%10.10f], (xl=%10.10f, xr=%10.10f)\n', iteration, a, b, xl, xr);
+            plot([xl xr], [fl fr], colors(1 + mod(iteration - 1, 3)));
+            plot(xl, fl, 'k.', 'MarkerSize', 10);
+            plot(xr, fr, 'k.', 'MarkerSize', 10);
+            iteration = iteration + 1;
         end
     end
 
